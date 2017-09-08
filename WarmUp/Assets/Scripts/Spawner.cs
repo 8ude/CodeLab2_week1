12e42621b4bedfff59c57d22ef6f045f9764a0af
8ude <crbertelsen@gmail.com>
2017-09-08 15:04:57 -0400

    git ignore not working, not surprising

---
+++ WarmUp/Assets/Scripts/Spawner.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour {
+
+	Sprite[] sprites;
+
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void SpawnSprite() {
+
+		GameObject goSprite = new GameObject();
+		goSprite.AddComponent<RigidBody2D>();
+	
+	}
+}
